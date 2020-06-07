e1252bd5a5764eec352643ffc3ae8e5ad9a93989
AdnanAsali <aasali40@gmail.com>
2020-06-07 20:52:22 +0300

    CRD Done, Update Left only

--- src/routes.js
+++ src/routes.js
@@ -16,13 +16,25 @@ var con = mysql.createConnection(
   });
 
 // Routing 
+
+// Getting all the meetings and the Teams' Data
 router.get('/', (req, res) => 
 {
-  con.query(
-    'SELECT teams.team_id, teams.name, fromDate, toDate, descr, room FROM meeting, teams order by teams.team_id asc'
-    , function (err, result) 
+  let query = 
+  `
+  SELECT 
+    teams.team_id, 
+    teams.name, fromDate, toDate, descr, room 
+      FROM meeting 
+        join teams on meeting.team_id = teams.team_id 
+          order by  
+            teams.team_id 
+              asc
+  `;
+  con.query(query , function (err, result) 
   {
-    
+    console.log(result);
+
     res.render('index', 
     {
       title: 'Display All Meetings',
@@ -32,6 +44,38 @@ router.get('/', (req, res) =>
   });
 });
 
+
+// Adding a new meeting
+
+router.post('/', (req, res) => 
+{
+  var desc = req.body.desc;
+  var room = req.body.room;
+  var from = req.body.from;
+  var to = req.body.to;
+  var team = req.body.team;
+
+
+  let query = 
+  ` 
+  INSERT INTO meeting (descr, fromDate, toDate, room, team_id)
+  VALUES ('${desc}', '${from}', '${to}', '${room}', '${team}');
+  `
+
+  con.query( query , function (err, result) 
+  {
+    if (err) {
+      return res.status(500).send(err);
+  }
+  res.redirect('/');
+  });
+});
+
+
+
+
+
+// Helping and Data Filtering/Processing Functions 
 var filterResult = (resJson) => 
 {
   
